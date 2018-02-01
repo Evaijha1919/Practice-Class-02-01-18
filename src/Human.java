befc716de7f13f3b4bbb4e57f86ffa2a2634e1c9
Hunter <tehunter.2017@mymail.jcsu.edu>
2018-02-01 13:52:14 -0500

    first commit

---
+++ src/Human.java
@@ -0,0 +1,19 @@
+
+public class Human {
+String firstname;
+String lastname;
+String middleinitial;
+String career;
+int year;
+
+
+public Human(String first, String last, String middle, String career1, int year1){
+		// TODO Auto-generated constructor stub}
+	firstname = first;
+	lastname =last;
+	middleinitial =middle;
+	career = career1;
+	 year= year1;
+}
+
+}
---
+++ src/humantest.java
@@ -0,0 +1,14 @@
+
+public class humantest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Human hum=new Human("Channel","Muldrow", "L","nurse", 2015);
+System.out.println(hum.firstname);
+System.out.println(hum.lastname);
+System.out.println(hum.middleinitial);
+System.out.println(hum.career);
+System.out.println(hum.year);
+	}
+
+}
